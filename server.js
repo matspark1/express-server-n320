91a398c4e6f675ba94490519677944caa126ee55
Matt S <matspark@iu.edu>
2025-04-01 16:15:16 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "views")));
+
+app.get("/*page", async (req, res) => {
+  res.sendFile(path.join(__dirname, "views/404.html"));
+});
+
+const port = process.env.PORT || 14962;
+
+app.listen(port);
+
+console.log(`Server running: http://localhost:${port}`);
