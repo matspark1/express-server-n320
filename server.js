490e417efc5761d66d3846566f14be3b8358c8ec
Matt S <matspark@iu.edu>
2025-04-01 17:10:18 -0400

    more fixes

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "views")));
 
-app.get("/*resource*/", function (req, res) {});
-
 app.get("/*page", async (req, res) => {
   res.sendFile(path.join(__dirname, "views/404.html"));
 });
